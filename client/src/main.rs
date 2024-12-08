5783143ed707ba9868706553bd9241656cfc5559
CPU_Blanc <g6yd96zk@cpublanc.mailer.me>
2024-12-08 21:30:51 +0000

    feat: initial config file support
    closes #1

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,72 +1,69 @@
 mod config;
 
+use clap::Parser;
+use config::{get_config_path, write_config_file, Args, Config};
+use is_empty::IsEmpty;
+use log::{debug, error, info, trace, warn, LevelFilter};
+use log4rs::{
+    append::console::ConsoleAppender,
+    config::{Appender, Config as LogConfig, Root},
+    encode::pattern::PatternEncoder,
+    filter::threshold::ThresholdFilter,
+};
 use sonarr_api::{
     queue::{DeleteQueueQuery, GetQueueQuery, QueueResource, QueueStatus},
     Sonarr,
 };
-use std::collections::HashSet;
 use std::{
-    env,
+    collections::HashSet,
+    process::exit,
     time::{Duration, Instant},
 };
 use tokio::time::sleep;
-
-use log::{debug, error, info, trace, warn, LevelFilter};
-use log4rs::{
-    append::console::ConsoleAppender,
-    config::{Appender, Config, Root},
-    encode::pattern::PatternEncoder,
-    filter::threshold::ThresholdFilter,
-};
-
 #[tokio::main]
 async fn main() {
-    let mut args = config::Args::from_args_and_env();
+    let args = Args::parse();
+    let used_cli = !args.is_empty();
+    let config_path = get_config_path();
 
-    let encoder = PatternEncoder::new("{h({d(%Y-%m-%d %H:%M:%S)} | {({l}):5.5} | {D({f}:{L} - )}{m}{n})}");
-    let stdout = ConsoleAppender::builder()
-        .encoder(Box::new(encoder))
-        .build();
-    let config = Config::builder()
-        .appender(
-            Appender::builder()
-                .filter(Box::new(ThresholdFilter::new(LevelFilter::Trace)))
-                .build("stdout", Box::new(stdout)),
-        )
-        .build(
-            Root::builder()
-                .appender("stdout")
-                .build(LevelFilter::from(args.log_level)),
-        )
-        .expect("error building logger");
-    let _ = log4rs::init_config(config).expect("error building logger");
-
-    info!("Scrubarr v{}", env!("CARGO_PKG_VERSION"));
-
-    //TODO: Deprecate port
-    let sonarr_url = if let Some(port) = args.port {
-        warn!("SCRUBARR_SONARR_PORT is deprecated and will be removed soon. Include the port in SCRUBARR_SONARR_URL");
-        args.url
-            .set_port(Some(port))
-            .expect("failed to map port to url");
-        args.url.to_string()
-    } else {
-        args.url.to_string()
+    let config = match Config::new(config_path.to_str().unwrap()) {
+        Ok(mut loaded) => {
+            if used_cli {
+                loaded.merge(args);
+            };
+            build_logger(loaded.log_level);
+            loaded
+        }
+        Err(e) => {
+            let mut new_config = Config::default();
+            if used_cli {
+                new_config.merge(args);
+            };
+            build_logger(new_config.log_level);
+            error!("error loading config - {e}");
+            new_config
+        }
     };
 
-    if args.omit_port.is_some() {
-        warn!("SCRUBARR_OMIT_PORT is deprecated and will be removed soon")
+    if used_cli {
+        warn!("use of CLI arguments is depreciated - edit the configuration file ({}) or set environmental variables instead", config_path.to_str().unwrap());
+        write_config_file(&config_path, &config);
     };
+
+    if config.sonarr_key.is_none() {
+        error!("no Sonarr API key configured - Set it in either the configuration file ({}) or SCRUBARR_SONARR_KEY env variable", config_path.to_str().unwrap());
+        exit(1);
+    }
 
     let sonarr = sonarr_api::new(
-        &args.api_key,
-        &sonarr_url,
-        args.base_path.as_deref(),
-        args.verbose,
+        config.sonarr_key.unwrap().as_ref(),
+        config.sonarr_url.as_ref(),
+        config.sonarr_base_path.as_deref(),
+        config.verbose,
     )
     .expect("error creating Sonarr client");
 
-    let config_interval = Duration::from_secs(std::cmp::max(args.interval, 300));
+    let config_interval = Duration::from_secs(std::cmp::max(config.interval, 300));
 
     let mut start_time;
 
@@ -147,4 +144,20 @@ async fn process_queue(client: &Sonarr) -> (HashSet<Option<i32>>, Vec<i32>) {
     }
 
     (tbas, replaced)
+}
+
+fn build_logger(level: LevelFilter) {
+    let encoder = PatternEncoder::new("{h({d(%Y-%m-%d %H:%M:%S)} | {({l}):5.5} | {D({f}:{L} - )}{m}{n})}");
+    let stdout = ConsoleAppender::builder()
+        .encoder(Box::new(encoder))
+        .build();
+    let config = LogConfig::builder()
+        .appender(
+            Appender::builder()
+                .filter(Box::new(ThresholdFilter::new(LevelFilter::Trace)))
+                .build("stdout", Box::new(stdout)),
+        )
+        .build(Root::builder().appender("stdout").build(level))
+        .expect("error building logger");
+    let _ = log4rs::init_config(config).expect("error building logger");
 }
